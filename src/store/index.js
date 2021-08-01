944a0e52d7050c67b0dd32c82e4bf7e189f8ab99
Usman Kiani <usmankiani256@gmail.com>
2021-08-02 02:34:03 +0500

    add redux store configurations

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { applyMiddleware, createStore } from 'redux'
+
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './Reducers'
+import thunk from 'redux-thunk'
+
+const initialState = {}
+const middleware = [thunk]
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
