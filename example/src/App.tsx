6c9f39c2c986d39832be465f0423999d1a6a9ace
Usman Kiani <usmankiani256@gmail.com>
2021-08-03 15:39:22 +0500

    update cancel button

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,11 +4,17 @@ import { PopupAlert, showPopup } from 'react-native-popup-alert'
 const App = () => {
   useEffect(() => {
     showPopup({
-      type: 'success',
+      type: 'error',
       autoDismiss: false,
       title: 'Custom Alert',
       body: 'This is a custom alert for testing the Alert component',
       buttons: [
+        {
+          name: 'Cancel',
+          onPress: () => {
+            showPopup(null)
+          },
+        },
         {
           name: 'Okay',
           onPress: () => {
