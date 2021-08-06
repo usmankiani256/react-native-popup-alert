7822619713a23e94ba05ac8b424912bf637d5e48
Usman Kiani <usmankiani256@gmail.com>
2021-08-06 15:02:46 +0500

    remove redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react'
 import Alert, { showPopup } from './components/PopupAlert/index.js'
-import { Provider } from 'react-redux'
 import { Provider as PaperProvider } from 'react-native-paper'
-import store from './store'
 
 const PopupAlert = () => {
   return (
-    <Provider store={store}>
-      <PaperProvider>
-        <Alert />
-      </PaperProvider>
-    </Provider>
+    <PaperProvider>
+      <Alert />
+    </PaperProvider>
   )
 }
 
