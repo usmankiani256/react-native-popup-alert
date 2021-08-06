c2771ac59af4ce91e90ae94abf7372a56c2ed5a0
Usman Kiani <usmankiani256@gmail.com>
2021-08-06 16:30:30 +0500

    update showPopup in example/src

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,8 +4,10 @@ import { PopupAlert, showPopup } from 'react-native-popup-alert'
 const App = () => {
   useEffect(() => {
     showPopup({
-      type: 'error',
-      autoDismiss: false,
+      icon: 'alert-circle',
+      iconSize: 70,
+      iconColor: 'orange',
+      // autoDismiss: 5,
       title: 'Custom Alert',
       body: 'This is a custom alert for testing the Alert component',
       buttons: [
