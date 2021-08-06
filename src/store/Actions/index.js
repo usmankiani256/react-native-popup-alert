045959c5280444163beead6a9396cadde996d817
Usman Kiani <usmankiani256@gmail.com>
2021-08-06 15:01:42 +0500

    remove redux store

--- src/store/Actions/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ShowAlert } from './ShowAlert'
--- src/store/Reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import ShowAlert from './ShowAlert'
-
-export default combineReducers({
-  ShowAlert: ShowAlert,
-})
