3fec7462c9ce6572263f576371d650aae6c56f7f
Usman Kiani <usmankiani256@gmail.com>
2021-08-10 12:47:23 +0500

    update component structure

--- src/components/PopupAlert/index.js
+++ src/components/PopupAlert/index.js
@@ -3,35 +3,8 @@ import { Button, Dialog, Portal, IconButton } from 'react-native-paper'
 import { View, Text, ScrollView } from 'react-native'
 import styles from './styles'
 
-var emitter = require('tiny-emitter/instance')
-
-// let samplePayload = {
-//   icon: 'camera',
-//   iconColor: 'green',
-//   iconSize: 25,
-//   autoDismiss: 5,
-//   title: 'Custom Alert Message',
-//   body: 'This is a custom alert message',
-//   buttons: [
-//     {
-//       name: 'Okay',
-//       onPress: () => {},
-//     },
-//   ],
-// }
-
-export const showPopup = (payload) => {
-  emitter.emit('Show_Alert_Fired', payload)
-}
-
 const Alert = (props) => {
-  const [alert, setAlert] = React.useState(null)
-
-  React.useEffect(() => {
-    emitter.on('Show_Alert_Fired', function (payload) {
-      setAlert(payload)
-    })
-  }, [])
+  const { alert, hideDialog } = props
 
   React.useEffect(() => {
     if (alert && alert.autoDismiss && alert.autoDismiss > 0) {
@@ -43,19 +16,19 @@ const Alert = (props) => {
     }
   }, [alert])
 
-  function hideDialog() {
-    setAlert(null)
-  }
-
-  if (!alert) {
+  if (JSON.stringify(alert) === '{}') {
     return null
   }
 
   let title = alert.title || 'Custom Alert'
   let body = alert.body || 'This is a custom alert description'
+  let icon = alert.icon
+  let iconColor = alert.iconColor
+  let iconSize = alert.iconSize
+  let buttons = alert.buttons
 
   return (
-    <Portal>
+    <Portal testID='alert'>
       <Dialog
         dismissable={false}
         style={styles.root}
@@ -66,20 +39,20 @@ const Alert = (props) => {
         <ScrollView>
           <View style={styles.content}>
             <View style={styles.iconContainer}>
-              {alert.icon && (
+              {icon && (
                 <IconButton
-                  icon={alert.icon}
-                  color={alert.iconColor || 'grey'}
-                  size={alert.iconSize || 30}
+                  icon={icon}
+                  color={iconColor || 'grey'}
+                  size={iconSize || 30}
                 />
               )}
             </View>
             <Text style={styles.body}>{body}</Text>
           </View>
-          {alert.buttons && (
+          {buttons && (
             <Dialog.Actions>
-              {alert.buttons.map(({ name, onPress }, index) => {
-                let len = alert.buttons.length
+              {buttons.map(({ name, onPress }, index) => {
+                let len = buttons.length
 
                 return (
                   <Button
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react'
-import Alert, { showPopup } from './components/PopupAlert/index.js'
-import { Provider as PaperProvider } from 'react-native-paper'
+import Alert from './components/PopupAlert/index.js'
+import { Provider } from 'react-native-paper'
+
+var emitter = require('tiny-emitter/instance')
 
 const PopupAlert = () => {
+  const [alert, setAlert] = React.useState({})
+
+  React.useEffect(() => {
+    emitter.on('Show_Alert_Fired', function (payload: Object) {
+      setAlert(payload)
+    })
+  }, [])
+
+  function hideDialog() {
+    setAlert({})
+  }
+
   return (
-    <PaperProvider>
-      <Alert />
-    </PaperProvider>
+    <Provider>
+      <Alert alert={alert} hideDialog={hideDialog} />
+    </Provider>
   )
+}
+
+const showPopup = (payload: Object) => {
+  emitter.emit('Show_Alert_Fired', payload)
 }
 
 export { PopupAlert, showPopup }
